cdd45831df50522bd359cae0626d5b50d2826550
Syzilgy <catlastowka@gmail.com>
2016-09-12 11:54:51 -0400

    BALLS

---
+++ Assets/Scripts/NewBehaviourScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var points : int = 5;
+public var pickedUpBy : String = "Player";
+
+function OnTriggerEnter2D(other : Collider2D)
+{
+
+	if ( other.CompareTaf(pickedUpBy) )
+	{
+		Debug.Log("Coins! Worth " + points + "points!");
+
+		Destroy(gameObject);
+
+		}
+
+	}
