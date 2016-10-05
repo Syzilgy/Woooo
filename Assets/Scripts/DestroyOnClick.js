51f93fe0d18de4594dbea88ce570b8d776163c79
Syzilgy <catlastowka@gmail.com>
2016-10-05 13:23:32 -0400

    IT WORK
    o

--- Assets/Scripts/DestroyOnClick.js
+++ Assets/Scripts/DestroyOnClick.js
@@ -4,6 +4,8 @@ function OnMouseDown ()
 {
 	if (gameObject.transform.position.y > -4 && gameObject.transform.position.y < 4)
 	{
+		GameData.numGrabbed++;
+		GameData.DisplayScore();
 		Destroy(gameObject);
 	}
 }
